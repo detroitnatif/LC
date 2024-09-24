dbef0c9078227fdfa85a88c647a9b0717192e05b
detroitnatif <tytydizzle124@gmail.com>
2024-09-24 14:24:10 -0400

    not working - 3 pointers

---
+++ triplet_sequence.py
@@ -0,0 +1,26 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+
+        f, m, l = 0, 0, 0
+
+        for i in range(len(nums)):
+            if nums[i] < nums[f]:
+                print("first")
+                f = i
+                m = i
+                l = i
+            elif nums[i] > nums[f]:
+                print('middle')
+                if nums[i] > nums[m]:
+                    m = i
+                if nums[i] > nums[l]:
+                    l = i
+            elif nums[i] > nums[m]:
+                print('last')
+                l = i
+
+
+        if f < m and m < l:
+            return True
+        else:
+            return False
