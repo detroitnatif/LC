9ab0e3c82b9967db465a754cfdbb1a0f4f3d2927
detroitnatif <tyler.klimas@duke.edu>
2024-10-16 08:09:10 -0400

    SQL

---
+++ temperature.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+select w1.id 
+from Weather w1
+join Weather w2
+on Date_sub(w1.recordDate, interval 1 day) = w2.recordDate
+where w1.temperature > w2.temperature
