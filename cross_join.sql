81ac2c447f0324a5b7fdf21fefab10b69a290761
detroitnatif <tyler.klimas@duke.edu>
2024-10-24 12:48:22 -0400

    cross join - joins all combinations

---
+++ cross_join.sql
@@ -0,0 +1,9 @@
+select Students.student_id, Students.student_name,
+Subjects.subject_name, COUNT(Examinations.subject_name) as attended_exams
+from Students
+cross join Subjects
+left join Examinations 
+on Students.student_id = Examinations.student_id
+and Subjects.subject_name = Examinations.subject_name
+group by Students.student_id, Students.student_name, Subjects.subject_name
+order by Students.student_id ASC
